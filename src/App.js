5cb4164017a8148589226a32b506de70076a4296
igmehandzhiev <imehandzhiev@intracol.com>
2019-06-01 11:23:23 +0300

    Add example gauge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactSpeedometer from "react-d3-speedometer";
+import logo from "./resources/logo_black.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ReactSpeedometer
+          maxValue={500}
+          value={333}
+          startColor="green"
+          endColor="red"
+          maxSegmentLabels={5}
+          segments={1000}
+          needleTransitionDuration={4000}
+          needleTransition="easeElastic"
+        />
       </header>
     </div>
   );
