62604539a5e6d26f562d38d5bfa1bdfa053f1268
igmehandzhiev <imehandzhiev@intracol.com>
2019-06-07 19:51:23 +0300

    Add buld/

---
+++ build/precache-manifest.1748f67923fce073f72287df799be1c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11daa7833a8d6554259e343d1e56e8e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79d1724188a039ef2f38",
+    "url": "/static/css/main.0cfe834d.chunk.css"
+  },
+  {
+    "revision": "e7447c5a1150d648d3d6",
+    "url": "/static/js/2.f953483b.chunk.js"
+  },
+  {
+    "revision": "79d1724188a039ef2f38",
+    "url": "/static/js/main.6561ce38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "84bb471cb51d21e690cffc631e6cedad",
+    "url": "/static/media/logo_white.84bb471c.png"
+  }
+]);
