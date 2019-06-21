5fe5aee1b3d6f502369de57e284d81dc731405de
Ivan Mehandzhiev <ivan.mehandzhiev@trading212.com>
2019-06-21 16:12:15 +0300

    Extract chart options

--- src/components/DiseasesPredictionsChart.jsx
+++ src/components/DiseasesPredictionsChart.jsx
@@ -1,75 +1,100 @@
 import React from "react";
 import ReactApexChart from "react-apexcharts";
 
+const chartOptions = {
+  chart: {
+    id: "mychart",
+    shadow: {
+      enabled: true,
+      color: "#000",
+      top: 18,
+      left: 7,
+      blur: 10,
+      opacity: 1
+    },
+    toolbar: {
+      show: false
+    }
+  },
+  colors: ["#77B6EA", "#545454"],
+  dataLabels: {
+    enabled: true
+  },
+  stroke: {
+    curve: "smooth"
+  },
+  title: {
+    text: "Diseases predictions",
+    style: {
+      fontSize: "26px"
+    },
+    align: "left"
+  },
+  grid: {
+    borderColor: "#e7e7e7",
+    row: {
+      colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+      opacity: 0.5
+    }
+  },
+  markers: {
+    size: 6
+  },
+  xaxis: {
+    // Awful, but working as we want (:
+    categories: [
+      `${new Date().getDate() 
+        }.${ 
+        new Date().getMonth() + 1 
+        }.${ 
+        new Date().getFullYear()}`,
+      `${new Date().getDate() +
+        1 
+        }.${ 
+        new Date().getMonth() + 1 
+        }.${ 
+        new Date().getFullYear()}`,
+      `${new Date().getDate() +
+        2 
+        }.${ 
+        new Date().getMonth() + 1 
+        }.${ 
+        new Date().getFullYear()}`,
+      `${new Date().getDate() +
+        3 
+        }.${ 
+        new Date().getMonth() + 1 
+        }.${ 
+        new Date().getFullYear()}`,
+      `${new Date().getDate() +
+        4 
+        }.${ 
+        new Date().getMonth() + 1 
+        }.${ 
+        new Date().getFullYear()}`
+    ],
+    title: {
+      text: "Date"
+    }
+  },
+  yaxis: {
+    min: 0,
+    max: 100
+  },
+  legend: {
+    position: "top",
+    horizontalAlign: "right",
+    floating: true,
+    offsetY: -25,
+    offsetX: -5
+  }
+};
+
 export class DiseasesPredictionsChart extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      options: {
-        chart: {
-          id: "mychart",
-          shadow: {
-            enabled: true,
-            color: "#000",
-            top: 18,
-            left: 7,
-            blur: 10,
-            opacity: 1
-          },
-          toolbar: {
-            show: false
-          }
-        },
-        colors: ["#77B6EA", "#545454"],
-        dataLabels: {
-          enabled: true
-        },
-        stroke: {
-          curve: "smooth"
-        },
-        title: {
-          text: "Diseases predictions",
-          style: {
-            fontSize: "26px"
-          },
-          align: "left"
-        },
-        grid: {
-          borderColor: "#e7e7e7",
-          row: {
-            colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-            opacity: 0.5
-          }
-        },
-        markers: {
-          size: 6
-        },
-        xaxis: {
-        // Awful, but working as we want (:
-          categories: [
-            new Date().getDate() + "." + (new Date().getMonth() + 1) + "." + new Date().getFullYear(),
-            new Date().getDate() + 1 + "." + (new Date().getMonth() + 1) + "." + new Date().getFullYear(),
-            new Date().getDate() + 2 + "." + (new Date().getMonth() + 1) + "." + new Date().getFullYear(),
-            new Date().getDate() + 3 + "." + (new Date().getMonth() + 1) + "." + new Date().getFullYear(),
-            new Date().getDate() + 4 + "." + (new Date().getMonth() + 1) + "." + new Date().getFullYear()
-          ],
-          title: {
-            text: "Date"
-          }
-        },
-        yaxis: {
-          min: 0,
-          max: 100
-        },
-        legend: {
-          position: "top",
-          horizontalAlign: "right",
-          floating: true,
-          offsetY: -25,
-          offsetX: -5
-        }
-      },
       series: [
         {
           name: "Mildew Index",
@@ -84,12 +109,12 @@ export class DiseasesPredictionsChart extends React.Component {
   }
 
   render() {
-    const { options, series } = this.state;
+    const { series } = this.state;
 
     return (
       <div id="chart">
         <ReactApexChart
-          options={options}
+          options={chartOptions}
           series={series}
           type="line"
           height="400"
